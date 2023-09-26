780f60557bf813118e9a926ebb5d19dbf3103cff
Kaique_vv <malaquiask2@gmail.com>
2023-09-26 00:13:52 -0300

    new atualização
    mudando algumas coisas

--- js/game.js
+++ js/game.js
@@ -1,17 +1,9 @@
 const grid =document.querySelector('.grid');
 
 const chacracters =[
-  'Aqua',
-  'Aqua2',
-  'carinha',
-  'charmander',
   'chopper',
   'luffy',
-  'rick',
-  'pickle-rick',
   'Pikachu',
-  'Power',
-  'Megumisad',
   'one 1',
   'one 2',
   'one 3',
@@ -30,6 +22,66 @@ const createElement = (tag, className)=>{
     element.className = className;
     return  element;
 }
+let PrimeiraC='';
+let SegudaC ='';
+
+const fimDoJogo=()=>{
+  const cartaAcertada = document.querySelectorAll('.acertouCarta');
+
+  if(cartaAcertada.length ===28){
+    alert('Parabens, você encontrou o One piece!!');
+  }
+}
+const confiraCartas =()=>{
+
+
+  const  firstCharacter = PrimeiraC.getAttribute('data-chacracters');
+  const  SecondCharacter = SegudaC.getAttribute('data-chacracters');
+
+
+  if(firstCharacter ===SecondCharacter) {
+
+
+    PrimeiraC.firstChild.classList.add('acertouCarta');
+    SegudaC.firstChild.classList.add('acertouCarta');
+
+
+    PrimeiraC='';
+    SegudaC ='';
+
+
+    fimDoJogo();
+
+}else{
+
+  setTimeout(()=>{
+    PrimeiraC.classList.remove('revela-carta');
+    SegudaC.classList.remove('revela-carta');
+
+     PrimeiraC='';
+     SegudaC ='';
+
+  } ,500)
+  
+}
+}
+
+const revealCard =({target})=>{
+  if(target.parentNode.className.includes('revela-carta'))
+  {
+    return;
+  }
+  if(PrimeiraC===''){
+    target.parentNode.classList.add('revela-carta');
+    PrimeiraC= target.parentNode;
+  }else if(SegudaC ===''){
+    target.parentNode.classList.add('revela-carta');
+    SegudaC= target.parentNode;
+
+    confiraCartas();
+  }
+    
+}
 const createCard = (chacracters) => {
 
     const card = createElement('div', 'card');
@@ -42,13 +94,17 @@ const createCard = (chacracters) => {
     card.appendChild(back);
   
 
-  
+    card.addEventListener('click', revealCard)
+    card.setAttribute('data-chacracters', chacracters);
     return card;
   }
 
-const loadGame =()=>{
-    chacracters.forEach((chacracters) => {
+const loadGame = ()=>{
+  const duplicateCharacters =[ ...chacracters,...chacracters ];
 
+  const shuffleArray = duplicateCharacters.sort(()=> Math.random()-0.5);
+  
+  shuffleArray.forEach((chacracters) => {
         const card = createCard(chacracters);
         grid.appendChild(card);
 
